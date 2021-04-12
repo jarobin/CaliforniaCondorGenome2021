b30866df0c4e666f25f0db281e5b009a2eeeec92
Jacqueline Robinson <37120939+jarobin@users.noreply.github.com>
2021-04-11 22:21:19 -0700

    Update busco_aves.sh

--- busco_aves.sh
+++ busco_aves.sh
@@ -1,7 +1,10 @@
 # BUSCO
 # Used v4.0.5 and v4.0.6 through conda
 # Dataset used for analysis: https://busco-data.ezlab.org/v4/data/lineages/aves_odb10.2019-11-20.tar.gz
-
+#
+# Results for various avian assemblies provided in Table S1.
+#
+# Example usage:
 export FASTA=galGal6.fa
 export LINEAGE=aves_odb10
 busco --cpu 8 -m genome --augustus_species=chicken -i ${FASTA} -o busco_${FASTA%.fa*} -l ${LINEAGE}
